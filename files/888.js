9b1ec799881fa7c50bce4c055fdf8ecb6160b234
jxjy80088 <35170758+jxjy80088@users.noreply.github.com>
2019-07-07 17:44:41 +0800

    Add files via upload

---
+++ files/888.js
@@ -0,0 +1,31 @@
+
+var _url = 'http://106manbetx.com/';
+if ((navigator.userAgent.match(/(iPhone|iPod|Android|ios|mobile)/i)))
+{
+	_url = 'http://m.106manbetx.com/';
+}
+
+
+$(".clicktz").click(function(){
+    window.location=_url;
+});
+$("#zhuceid").click(function(){
+    window.location=_url;
+});
+$("#enter-click").click(function(){
+    window.location=_url;
+});
+$("figure").click(function(){
+    window.location=_url;
+});
+$("#zhuceid2").click(function(){
+    window.location=_url;
+});
+
+
+
+
+$(function()
+{
+
+});
